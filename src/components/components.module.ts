b4bd0f647699fcac054bddee70065b479418e2b0
Juan José Suárez <suarezramirezjuanjose@hotmail.com>
2020-04-17 08:13:55 +0200

    add footer

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,14 +3,17 @@ import { WritterComponent } from './writter/writter';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from 'ionic-angular';
 import { PipesModule } from '../pipes/pipes.module';
+import { VitalittyFooterComponent } from './vitalitty-footer/vitalitty-footer';
 
 @NgModule({
-	declarations: [WritterComponent],
+	declarations: [WritterComponent,
+    VitalittyFooterComponent],
 	imports: [
     IonicModule,
     TranslateModule.forChild(),
     PipesModule
   ],
-	exports: [WritterComponent]
+	exports: [WritterComponent,
+    VitalittyFooterComponent]
 })
 export class ComponentsModule {}
---
+++ src/components/vitalitty-footer/vitalitty-footer.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavHelperProvider } from './../../providers/ui/nav-helper';
+
+/**
+ * Generated class for the VitalittyFooterComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'vitalitty-footer',
+  templateUrl: 'vitalitty-footer.html',
+})
+export class VitalittyFooterComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello VitalittyFooterComponent Component');
+    this.text = 'Hello World';
+  }
+
+  // public goToPage(page: string) {
+  //   if (!!page && !page.includes('http')) {
+  //     this.navHelper.push(page);
+  //   } else if (!!page) {
+  //     this.navHelper.goToLink(page);
+  //   }
+  // }
+
+}
